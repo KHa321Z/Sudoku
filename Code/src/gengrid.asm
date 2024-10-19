7e75053e754b0a631645fb029ef647bac1d7c467
KHa321Z <khammadzaeem@gmail.com>
2024-10-19 17:14:03 +0500

    Added Print and Grid Functions

--- Code/src/gengrid.asm
+++ Code/src/gengrid.asm
@@ -68,3 +68,120 @@ fill_loop:
     POP BP
 
     RET 12
+
+
+traverseGrid:
+    ; [BP + 14] POS_X (RETURN)
+    ; [BP + 12] POS_Y (RETURN)
+    ; [BP + 10] IND_X
+    ; [BP + 08] IND_Y
+    ; [BP + 06] FILL_ARRAY
+    ; [BP + 04] BOX_SIZE
+    PUSH BP
+    MOV BP, SP
+
+    PUSH CX
+    PUSH DX
+    PUSH SI
+
+    MOV SI, [BP + 6]
+    MOV CX, [SI]
+    MOV DX, [SI]
+    ADD SI, 2
+
+traverse_rows:
+    CMP word [BP + 10], 0
+    JZ set_cols
+
+    ADD CX, [BP + 4]
+    ADD CX, [SI]
+    ADD SI, 2
+    DEC word [BP + 10]
+
+    JMP traverse_rows
+
+set_cols:
+    MOV SI, [BP + 6]
+    ADD SI, 2
+
+traverse_cols:
+    CMP word [BP + 8], 0
+    JZ set_rows_cols
+
+    ADD DX, [BP + 4]
+    ADD DX, [SI]
+    ADD SI, 2
+    DEC word [BP + 8]
+
+    JMP traverse_cols
+
+set_rows_cols:
+    MOV [BP + 14], CX
+    MOV [BP + 12], DX
+
+    POP SI
+    POP DX
+    POP CX
+
+    MOV SP, BP
+    POP BP
+
+    RET 8
+
+clearGridBox:
+    ; [BP + 16] IND_X
+    ; [BP + 14] IND_Y
+    ; [BP + 12] GRID_X
+    ; [BP + 10] GRID_Y
+    ; [BP + 08] FILL_ARRAY
+    ; [BP + 06] BOX_SIZE
+    ; [BP + 04] COLOR
+    PUSH BP
+    MOV BP, SP
+
+    PUSHA
+
+    SUB SP, 4
+    PUSH word [BP + 16]
+    PUSH word [BP + 14]
+    PUSH word [BP + 8]
+    PUSH word [BP + 6]
+    CALL traverseGrid
+    POP DX
+    POP CX
+
+    MOV SI, [BP + 8]
+    ADD CX, [BP + 12]
+    ADD DX, [BP + 10]
+
+    MOV SI, CX
+    ADD SI, [BP + 6]
+    MOV DI, DX
+    ADD DI, [BP + 6]
+    PUSH CX
+
+    MOV AH, 0x0C
+    MOV AL, [BP + 4]
+    MOV BX, 0
+
+clear_box:
+    INT 0x10
+
+    INC CX
+    CMP CX, SI
+    JL clear_box
+
+    POP CX
+    PUSH CX
+    INC DX
+
+    CMP DX, DI
+    JL clear_box
+
+    POP CX
+    POPA
+
+    MOV SP, BP
+    POP BP
+
+    RET 14
--- Code/src/printf.asm
+++ Code/src/printf.asm
@@ -66,3 +66,142 @@ same_row:
     POP BP
 
     RET 12
+
+
+printNotes:
+    ; [BP + 14] IND_X
+    ; [BP + 12] IND_Y
+    ; [BP + 10] GRID_X
+    ; [BP + 08] GRID_Y
+    ; [BP + 06] FILL_ARRAY
+    ; [BP + 04] BOX_SIZE
+    PUSH BP
+    MOV BP, SP
+
+    PUSHA
+
+    MOV SI, notes_values
+
+    ; TRAVERSING COL INDICES IN NOTES
+    MOV AX, 18
+    MUL byte [BP + 12]
+    ADD SI, AX
+    ; TRAVERSING ROW INDICES IN NOTES
+    MOV AX, [BP + 14]
+    SHL AX, 1
+    ADD SI, AX
+
+    SUB SP, 4
+    PUSH word [BP + 14]
+    PUSH word [BP + 12]
+    PUSH word [BP + 6]
+    PUSH word [BP + 4]
+    CALL traverseGrid
+    POP DX
+    POP CX
+
+    ADD CX, [BP + 10]
+    ADD CX, 5
+    ADD DX, [BP + 8]
+    ADD DX, 5
+    MOV AX, [SI]
+    MOV DI, smol + 8
+    MOV BH, 9
+    MOV BL, 3
+    PUSH CX
+
+draw_note:
+    SHL AX, 1
+    JNC skip_note
+
+    PUSH CX
+    PUSH DX
+    PUSH word 8
+    PUSH word 8
+    PUSH word 0xF
+    PUSH DI
+    CALL printfont
+
+skip_note:
+    ADD CX, 12
+    ADD DI, 8
+
+    DEC BH
+    JZ terminate_printNotes
+
+same_note:
+    DEC BL
+    JNZ draw_note
+
+    POP CX
+    PUSH CX
+    ADD DX, 12
+    MOV BL, 3
+
+    JMP draw_note
+
+terminate_printNotes:
+    POP CX
+    POPA
+
+    MOV SP, BP
+    POP BP
+
+    RET 12
+
+
+
+printNumbers:
+    ; [BP + 14] IND_X
+    ; [BP + 12] IND_Y
+    ; [BP + 10] GRID_X
+    ; [BP + 08] GRID_Y
+    ; [BP + 06] FILL_ARRAY
+    ; [BP + 04] BOX_SIZE
+    PUSH BP
+    MOV BP, SP
+
+    PUSHA
+
+    MOV SI, grid_values
+    MOV DI, big
+
+    MOV AX, 9
+    MUL byte [BP + 12]
+    ADD SI, AX
+    ADD SI, [BP + 14]
+
+    SUB SP, 4
+    PUSH word [BP + 14]
+    PUSH word [BP + 12]
+    PUSH word [BP + 6]
+    PUSH word [BP + 4]
+    CALL traverseGrid
+    POP DX
+    POP CX
+
+    ADD CX, [BP + 10]
+    ADD CX, 9
+    ADD DX, [BP + 8]
+    ADD DX, 5
+
+    XOR AX, AX
+    MOV AL, [SI]
+    MOV BX, 96
+    MUL BL
+    ADD DI, AX
+
+    PUSH CX
+    PUSH DX
+    PUSH word 24
+    PUSH word 32
+    PUSH word 0x0
+    PUSH DI
+    CALL printfont
+
+    POPA
+
+    MOV SP, BP
+    POP BP
+
+    RET 12
